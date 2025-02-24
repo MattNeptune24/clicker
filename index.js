1809cd8318471c6eea30cf0b2b30529fc1894e18
MattNeptune24 <matthewsuchter41@icloud.com>
2025-02-24 17:29:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const totalDisplay = document.getElementById("Total")
 
 var multiplier = 1
 var clicks = -1
+var autoclicker = 0
+var price11 = 50000
 
 var price1 = 10
 var multiplier1 = 1
@@ -14,22 +16,23 @@ var multiplier3 = 50
 var price4 = 50000
 var multiplier4 = 100
 
-var price5 = 100000
+var price5 = 250000
 var multiplier5 = 500
-var price6 = 500000
+var price6 = 1000000
 var multiplier6 = 1000
 
-var price7 = 1000000
+var price7 = 5000000
 var multiplier7 = 5000
-var price8 = 5000000
+var price8 = 20000000
 var multiplier8 = 10000
 
-var price9 = 10000000
+var price9 = 100000000
 var multiplier9 = 50000
-var price10 = 50000000
+var price10 = 500000000
 var multiplier10 = 100000
 
 document.getElementById("Button").onclick = click
+document.getElementById("Autoclicker").onclick = purchase11
 document.getElementById("Shop1").onclick = purchase1
 document.getElementById("Shop2").onclick = purchase2
 document.getElementById("Shop3").onclick = purchase3
@@ -52,150 +55,184 @@ document.getElementById("Shop8").textContent = "+" + new Intl.NumberFormat("en-U
 document.getElementById("Shop9").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier9)
 document.getElementById("Shop10").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier10)
 
-
-
-
-function purchase1(){
-    if(clicks>=(price1-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price1)
-        clicks = clicks-price1
-        multiplier = multiplier + multiplier1
-        multiplier1 = multiplier1 + 1
-        document.getElementById("Shop1").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier1)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price1 = price1 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price1) + " clicks to purchase this upgrade."
+const autoclicker1 = (autoclicker) => {
+    setTimeout(() => {
+    clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks+multiplier)
+    clicks = clicks + multiplier
+    if(autoclicker >= 1){
+        autoclicker1(autoclicker)
     }
+    }, autoclicker * 1000);
 }
 
-function purchase2(){
-    if(clicks>=(price2-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price2)
-        clicks = clicks-price2
-        multiplier = multiplier + multiplier2
-        multiplier2 = multiplier2 + 2
-        document.getElementById("Shop2").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier2)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price2 = price2 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price2) + " clicks to purchase this upgrade."
-    }
-}
+autoclicker1(autoclicker)
 
-function purchase3(){
-    if(clicks>=(price3-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price3)
-        clicks = clicks-price3
-        multiplier = multiplier + multiplier3
-        multiplier3 = multiplier3 + 10
-        document.getElementById("Shop3").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier3)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price3 = price3 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price3) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase4(){
-    if(clicks>=(price4-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price4)
-        clicks = clicks-price4
-        multiplier = multiplier + multiplier4
-        multiplier4 = multiplier4 + 20
-        document.getElementById("Shop4").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier4)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price4 = price4 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price4) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase5(){
-    if(clicks>=(price5-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price5)
-        clicks = clicks-price5
-        multiplier = multiplier + multiplier5
-        multiplier5 = multiplier5 + 100
-        document.getElementById("Shop5").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier5)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price5 = price5 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price5) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase6(){
-    if(clicks>=(price6-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price6)
-        clicks = clicks-price6
-        multiplier = multiplier + multiplier6
-        multiplier6 = multiplier6 + 200
-        document.getElementById("Shop6").textContent = "+" + multiplier6
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price6 = price6 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price6) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase7(){
-    if(clicks>=(price7-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price7)
-        clicks = clicks-price7
-        multiplier = multiplier + multiplier7
-        multiplier7 = multiplier7 + 1000
-        document.getElementById("Shop7").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier7)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price7 = price7 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price7) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase8(){
-    if(clicks>=(price8-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price8)
-        clicks = clicks-price8
-        multiplier = multiplier + multiplier8
-        multiplier8 = multiplier8 + 2000
-        document.getElementById("Shop8").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier8)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price8 = price8 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price8) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase9(){
-    if(clicks>=(price9-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price9)
-        clicks = clicks-price9
-        multiplier = multiplier + multiplier9
-        multiplier9 = multiplier9 + 1000
-        document.getElementById("Shop9").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier9)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price9 = price9 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price9) + " clicks to purchase this upgrade."
-    }
-}
-
-function purchase10(){
-    if(clicks>=(price10-1)){
-        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price10)
-        clicks = clicks-price10
-        multiplier = multiplier + multiplier10
-        multiplier10 = multiplier10 + 2000
-        document.getElementById("Shop10").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier10)
-        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Clicks / Click"
-        price10 = price10 * 2
-    }else{
-        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price10) + " clicks to purchase this upgrade."
+function autoclicker0(){
+    console.log(autoclicker)
+    if(autoclicker >= 1){
+        click()
     }
 }
 
 function click(){
     clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks+multiplier)
     clicks = clicks + multiplier
+}
+
+function purchase1(){
+    if(clicks>=(price1)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price1)
+        clicks = clicks-price1
+        multiplier = multiplier + multiplier1
+        multiplier1 = multiplier1 + 1
+        document.getElementById("Shop1").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier1)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price1 = price1 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price1) + " points to purchase this upgrade."
+    }
+}
+
+function purchase2(){
+    if(clicks>=(price2)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price2)
+        clicks = clicks-price2
+        multiplier = multiplier + multiplier2
+        multiplier2 = multiplier2 + 2
+        document.getElementById("Shop2").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier2)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price2 = price2 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price2) + " points to purchase this upgrade."
+    }
+}
+
+function purchase3(){
+    if(clicks>=(price3)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price3)
+        clicks = clicks-price3
+        multiplier = multiplier + multiplier3
+        multiplier3 = multiplier3 + 10
+        document.getElementById("Shop3").textContent = "+" + multiplier3
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price3 = price3 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price3) + " points to purchase this upgrade."
+    }
+}
+
+function purchase4(){
+    if(clicks>=(price4)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price4)
+        clicks = clicks-price4
+        multiplier = multiplier + multiplier4
+        multiplier4 = multiplier4 + 20
+        document.getElementById("Shop4").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier4)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price4 = price4 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price4) + " points to purchase this upgrade."
+    }
+}
+
+function purchase5(){
+    if(clicks>=(price5)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price5)
+        clicks = clicks-price5
+        multiplier = multiplier + multiplier5
+        multiplier5 = multiplier5 + 100
+        document.getElementById("Shop5").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier5)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price5 = price5 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price5) + " points to purchase this upgrade."
+    }
+}
+
+function purchase6(){
+    if(clicks>=(price6)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price6)
+        clicks = clicks-price6
+        multiplier = multiplier + multiplier6
+        multiplier6 = multiplier6 + 200
+        document.getElementById("Shop6").textContent = "+" + multiplier6
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price6 = price6 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price6) + " points to purchase this upgrade."
+    }
+}
+
+function purchase7(){
+    if(clicks>=(price7)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price7)
+        clicks = clicks-price7
+        multiplier = multiplier + multiplier7
+        multiplier7 = multiplier7 + 1000
+        document.getElementById("Shop7").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier7)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price7 = price7 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price7) + " points to purchase this upgrade."
+    }
+}
+
+function purchase8(){
+    if(clicks>=(price8)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price8)
+        clicks = clicks-price8
+        multiplier = multiplier + multiplier8
+        multiplier8 = multiplier8 + 2000
+        document.getElementById("Shop8").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier8)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price8 = price8 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price8) + " points to purchase this upgrade."
+    }
+}
+
+function purchase9(){
+    if(clicks>=(price9)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price9)
+        clicks = clicks-price9
+        multiplier = multiplier + multiplier9
+        multiplier9 = multiplier9 + 1000
+        document.getElementById("Shop9").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier9)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price9 = price9 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price9) + " points to purchase this upgrade."
+    }
+}
+
+function purchase10(){
+    if(clicks>=(price10)){
+        clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price10)
+        clicks = clicks-price10
+        multiplier = multiplier + multiplier10
+        multiplier10 = multiplier10 + 2000
+        document.getElementById("Shop10").textContent = "+" + new Intl.NumberFormat("en-US").format(multiplier10)
+        document.getElementById("Text2").textContent = new Intl.NumberFormat("en-US").format(multiplier) + " Points / Click"
+        price10 = price10 * 2
+    }else{
+        document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price10) + " points to purchase this upgrade."
+    }
+}
+
+function purchase11(){
+    if(clicks>=(price11)){
+        if(autoclicker == 0){
+            clicksDisplay.textContent = new Intl.NumberFormat("en-US").format(clicks-price11)
+            clicks = clicks-price11
+            autoclicker = autoclicker + 1
+            document.getElementById("Autoclicker").className = "auto"
+            autoclicker1(autoclicker)
+        }
+    }else{
+        if(autoclicker == 0){
+            document.getElementById("Text").textContent = "You must have " + new Intl.NumberFormat("en-US").format(price11) + " Points to purchase this upgrade."
+        }else{
+            document.getElementById("Text").textContent = "This upgrade has reached it's highest level."
+        }
+    }
 }
